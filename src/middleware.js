e8616e9e2145ef852a69af67feef311e1cdd30b3
Jared Loson <jaredloson@gmail.com>
2019-08-12 22:59:53 -0700

    initial src

---
+++ src/middleware.js
@@ -0,0 +1,35 @@
+// Setting up an object where we will store any extra functionality 
+// that needs to run when performing particular actions on the app state.
+
+// The keys of this middleware object map to the `type` of an action.
+// When running that action.type, we check this object to see if there's
+// anything else we want to do. Look in `./reducer.js` to see it being used.
+
+export const middleware = {};
+
+
+// `registerMiddleware` is how we define any middleware functions we want
+// to run while updating the global state. We pass this function the name
+// of the action `type`, and the middleware function we want it to run.
+// The middleware functions are added as an array so we can define more than
+// one if necessary.
+
+export const registerMiddleware = ({ actionType, func }) => {
+  if (Array.isArray(middleware[actionType])) {
+    middleware[actionType].push(func);
+  } else {
+    middleware[actionType] = [func];
+  }
+}
+
+//Example Use:
+
+// registerMiddleware({
+//   actionType: "clickedAnnouncement",
+//   func: () => {
+//     // track Google Analytics event
+//     ga.send("clickedAnnouncement");
+//   }
+// });
+
+export default { middleware, registerMiddleware };
---
+++ src/reducer.js
@@ -0,0 +1,19 @@
+import { middleware } from "./middleware"; 
+
+export const bundleReducer = reducer => (state, action) => {
+  // First check if this action has any "middleware" functionality we want to run
+  if (Array.isArray(middleware[action.type])) {
+    middleware[action.type].forEach(func => func(action));
+  }
+
+  const newState = {};
+  // Then return the new object state based on the results
+  // of our individual reducers
+  Object.keys(reducer).forEach(key => {
+    newState[key] = reducer[key](state[key], action);
+  });
+
+  return newState;  
+};
+
+export default { bundleReducer };
